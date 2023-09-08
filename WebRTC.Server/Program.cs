60103194b0f4e723e49b7262d5e8ff41edc990ab
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-09-08 09:44:32 +0300

    Add project files.

---
+++ WebRTC.Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using SIPSorcery.Media;
+using SIPSorcery.Net;
+using SIPSorceryMedia.FFmpeg;
+using System.Net;
+using WebSocketSharp.Server;
+
+class Program
+{
+        private const int WEBSOCKET_PORT = 8081;
+
+        static void Main()
+        {
+            Console.WriteLine("WebRTC Get Started");
+
+            // Start web socket.
+            Console.WriteLine("Starting web socket server...");
+
+            //note: this REQUIRES ffmpeg 4.4 shared codecs in the output folder (only 4.4 version!)
+            FFmpegInit.Initialise(FfmpegLogLevelEnum.AV_LOG_VERBOSE, Environment.CurrentDirectory);
+
+            var webSocketServer = new WebSocketServer(IPAddress.Any, WEBSOCKET_PORT);
+            webSocketServer.AddWebSocketService<WebRTCWebSocketPeer>("/", (peer) => peer.CreatePeerConnection = () => CreatePeerConnection());
+            webSocketServer.Start();
+
+            Console.WriteLine($"Waiting for web socket connections on {webSocketServer.Address}:{webSocketServer.Port}...");
+            
+            Console.WriteLine("Press any key exit.");
+            Console.ReadKey();
+        }
+
+        private static Task<RTCPeerConnection> CreatePeerConnection()
+        {
+            var pc = new RTCPeerConnection(null);
+
+            var audioSource = new FFmpegFileSource("music.aac", false, new AudioEncoder(), audioFrameSize: 480, useVideo: false);
+            
+            var audioTrack = new MediaStreamTrack(audioSource.GetAudioSourceFormats(), MediaStreamStatusEnum.SendOnly);
+        
+            pc.addTrack(audioTrack);
+
+            audioSource.OnAudioSourceEncodedSample += pc.SendAudio; //wire sending as soon as data is received
+            audioSource.OnAudioSourceError += msg => pc.Close($"Audio Source Error! (got message '{msg}')");
+
+            pc.OnAudioFormatsNegotiated += (formats) => audioSource.SetAudioSourceFormat(formats.First());
+            pc.onconnectionstatechange += async (state) =>
+            {
+                Console.WriteLine($"Peer connection state change to {state}.");
+
+                switch(state)
+                {
+                    case RTCPeerConnectionState.connected:
+                        await audioSource.StartAudio();
+                        break;
+                    case RTCPeerConnectionState.failed:
+                        pc.Close("ice disconnection");
+                        break;
+                    case RTCPeerConnectionState.closed:
+                        await audioSource.CloseAudio();
+                        audioSource.Dispose();
+                        break;
+                }
+            };
+
+            return Task.FromResult(pc);
+        }
+
+}
