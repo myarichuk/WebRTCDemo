9ed35d18362282cc2bb646f2f71f6b83c2e055f2
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-09-09 15:30:51 +0300

    make webrtc stream from memory (streaming the file from memory via localhost url)

--- WebRTC.Server/Program.cs
+++ WebRTC.Server/Program.cs
@@ -2,38 +2,57 @@
 using SIPSorcery.Net;
 using SIPSorceryMedia.FFmpeg;
 using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WebSocketSharp.Server;
 
 class Program
 {
-        private const int WEBSOCKET_PORT = 8081;
+    private const int WEBSOCKET_PORT = 8081;
+    private const int HTTP_PORT = 5000;
 
-        static void Main()
-        {
-            Console.WriteLine("WebRTC Get Started");
+    static void Main()
+    {
+        // Start HTTP server for audio streaming
+        Task.Run(() => StartWebServer());
 
-            // Start web socket.
-            Console.WriteLine("Starting web socket server...");
+        // Your existing WebRTC and WebSocket code
+        Console.WriteLine("WebRTC Get Started");
+        Console.WriteLine("Starting web socket server...");
 
-            //note: this REQUIRES ffmpeg 4.4 shared codecs in the output folder (only 4.4 version!)
-            FFmpegInit.Initialise(FfmpegLogLevelEnum.AV_LOG_VERBOSE, Environment.CurrentDirectory);
+        FFmpegInit.Initialise(FfmpegLogLevelEnum.AV_LOG_VERBOSE, Environment.CurrentDirectory);
 
-            var webSocketServer = new WebSocketServer(IPAddress.Any, WEBSOCKET_PORT);
-            webSocketServer.AddWebSocketService<WebRTCWebSocketPeer>("/", (peer) => peer.CreatePeerConnection = () => CreatePeerConnection());
-            webSocketServer.Start();
+        var webSocketServer = new WebSocketServer(IPAddress.Any, WEBSOCKET_PORT);
+        webSocketServer.AddWebSocketService<WebRTCWebSocketPeer>("/", (peer) => peer.CreatePeerConnection = () => CreatePeerConnection());
+        webSocketServer.Start();
 
-            Console.WriteLine($"Waiting for web socket connections on {webSocketServer.Address}:{webSocketServer.Port}...");
-            
-            Console.WriteLine("Press any key exit.");
-            Console.ReadKey();
-        }
+        Console.WriteLine($"Waiting for web socket connections on {webSocketServer.Address}:{webSocketServer.Port}...");
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    private static void StartWebServer()
+    {
+        Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls($"http://*:{HTTP_PORT}");
+            })
+            .Build()
+            .Run();
+    }
 
         private static Task<RTCPeerConnection> CreatePeerConnection()
         {
             var pc = new RTCPeerConnection(null);
 
-            var audioSource = new FFmpegFileSource("music.aac", false, new AudioEncoder(), audioFrameSize: 480, useVideo: false);
-            
+            var audioSource = new FFmpegFileSource($"http://localhost:{HTTP_PORT}/audio", false, new AudioEncoder(), audioFrameSize: 480, useVideo: false);
+
             var audioTrack = new MediaStreamTrack(audioSource.GetAudioSourceFormats(), MediaStreamStatusEnum.SendOnly);
         
             pc.addTrack(audioTrack);
@@ -63,5 +82,38 @@ class Program
 
             return Task.FromResult(pc);
         }
+}
 
+public class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+}
+
+[ApiController]
+public class StreamController : ControllerBase
+{
+    [HttpGet("audio")]
+    public IActionResult GetAudioStream()
+    {
+        byte[] audioData = System.IO.File.ReadAllBytes("music.aac"); // Replace with your method to get audio data
+        MemoryStream memStream = new MemoryStream(audioData);
+
+        // Set the position to the beginning of the stream.
+        memStream.Seek(0, SeekOrigin.Begin);
+
+        return new FileStreamResult(memStream, "audio/aac");
+    }
 }
